8e1234b55d0475f029a01c7f3c36af162fb07f4a
dina.na <dina.na@bki.co.id>
2026-06-27 02:41:34 +0700

    chore(setup): configure Laravel 13 with Inertia Vue and Shadcn UI

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -16,11 +17,13 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
+            fn(Request $request) => $request->is('api/*'),
         );
     })->create();
--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,6 +1,192 @@
+import { router } from "@inertiajs/vue3";
 import { clsx } from "clsx";
+import { format, parseISO } from "date-fns";
+import { toast } from "vue-sonner";
 import { twMerge } from "tailwind-merge";
+import { id } from "date-fns/locale/id";
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+function cn(...inputs) {
+    return twMerge(clsx(inputs));
 }
+
+// FLASH MESSAGE
+function flashMessage(params) {
+    return params.props.flash_message;
+}
+
+// ACTION DELETE
+const deleteAction = (url, { closeModal, ...options } = {}) => {
+    const defaultOptions = {
+        preserveScroll: true,
+        preserveState: true,
+
+        onSuccess: (success) => {
+            const flash = flashMessage(success);
+
+            if (flash) {
+                toast[flash.type](flash.message);
+            }
+
+            if (closeModal && typeof closeModal === "function") {
+                closeModal();
+            }
+        },
+
+        ...options,
+    };
+
+    router.delete(url, defaultOptions);
+};
+
+// FORMAT DATE
+const formatDateIndo = (dateString) => {
+    if (!dateString) return "-";
+
+    return format(parseISO(dateString), "eeee, dd MMMM yyyy", {
+        locale: id,
+    });
+};
+
+// FORMAT IDR
+const formatToRupiah = (amount) => {
+    const formatter = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    });
+
+    return formatter.format(amount ?? 0);
+};
+
+const BUDGETTYPE = {
+    INCOME: "Penghasilan",
+    SAVING: "Tabungan dan Investasi",
+    DEBT: "Cicilan Hutang",
+    BIL: "Tagihan",
+    SHOPPING: "Belanja",
+};
+
+const BUDGETTYPEVARIANT = {
+    [BUDGETTYPE.INCOME]: "emerald",
+    [BUDGETTYPE.SAVING]: "orange",
+    [BUDGETTYPE.DEBT]: "red",
+    [BUDGETTYPE.BIL]: "sky",
+    [BUDGETTYPE.SHOPPING]: "purple",
+};
+
+const MONTHTYPE = {
+    JANUARI: "Januari",
+    FEBRUARI: "Februari",
+    MARET: "Maret",
+    APRIL: "April",
+    MEI: "Mei",
+    JUNI: "Juni",
+    JULI: "Juli",
+    AGUSTUS: "Agustus",
+    SEPTEMBER: "September",
+    OKTOBER: "Oktober",
+    NOVEMBER: "November",
+    DESEMBER: "Desember",
+};
+
+const MONTHTYPEVARIANT = {
+    [MONTHTYPE.JANUARI]: "fuchsia",
+    [MONTHTYPE.FEBRUARI]: "orange",
+    [MONTHTYPE.MARET]: "emerald",
+    [MONTHTYPE.APRIL]: "sky",
+    [MONTHTYPE.MEI]: "purple",
+    [MONTHTYPE.JUNI]: "rose",
+    [MONTHTYPE.JULI]: "pink",
+    [MONTHTYPE.AGUSTUS]: "red",
+    [MONTHTYPE.SEPTEMBER]: "violet",
+    [MONTHTYPE.OKTOBER]: "blue",
+    [MONTHTYPE.NOVEMBER]: "lime",
+    [MONTHTYPE.DESEMBER]: "teal",
+};
+
+const ASETTYPE = {
+    CASH: "Kas",
+    PERSONAL: "Personal",
+    SHORTTERM: "Investasi Jangka Pendek",
+    MIDTERM: "Investasi Jangka Tengah",
+    LONGTERM: "Investasi Jangka Panjang",
+};
+
+const ASETTYPEVARIANT = {
+    [ASETTYPE.CASH]: "emerald",
+    [ASETTYPE.PERSONAL]: "orange",
+    [ASETTYPE.SHORTTERM]: "red",
+    [ASETTYPE.MIDTERM]: "sky",
+    [ASETTYPE.LONGTERM]: "purple",
+};
+
+const LIABILITYTYPE = {
+    SHORTTERMDEBT: "Hutang Jangka Pendek",
+    MIDTERMDEBT: "Hutang Jangka Tengah",
+    LONGTERMDEBT: "Hutang Jangka Panjang",
+};
+
+const LIABILITYTYPEVARIANT = {
+    [LIABILITYTYPE.SHORTTERMDEBT]: "emerald",
+    [LIABILITYTYPE.MIDTERMDEBT]: "orange",
+    [LIABILITYTYPE.LONGTERMDEBT]: "red",
+};
+
+const LIABILITYDESCRIPTION = {
+    [LIABILITYTYPE.SHORTTERMDEBT]: "Teno",
+    [LIABILITYTYPE.MIDTERMDEBT]: "orange",
+    [LIABILITYTYPE.LONGTERMDEBT]: "red",
+};
+
+const message = {
+    503: {
+        title: "Service Unavailable",
+        description:
+            "Sorry, we are doing some maintenance. Please check back soon",
+        status: 503,
+    },
+    500: {
+        title: "Server Error",
+        description: "Oops, something went wrong",
+        status: 500,
+    },
+    404: {
+        title: "Not Found",
+        description: "Sorry, the page you are looking for could not be found",
+        status: 404,
+    },
+    403: {
+        title: "Forbidden",
+        description: "Sorry, you are forbidden from accessing this page",
+        status: 403,
+    },
+    401: {
+        title: "Unauthorized",
+        description: "Sorry, you are unauthorized to access this page",
+        status: 401,
+    },
+    429: {
+        title: "Too many requests",
+        description: "Please try again in just a second",
+        status: 429,
+    },
+};
+
+export {
+    cn,
+    flashMessage,
+    deleteAction,
+    formatDateIndo,
+    formatToRupiah,
+    BUDGETTYPE,
+    BUDGETTYPEVARIANT,
+    MONTHTYPE,
+    MONTHTYPEVARIANT,
+    ASETTYPE,
+    ASETTYPEVARIANT,
+    LIABILITYTYPE,
+    LIABILITYTYPEVARIANT,
+    LIABILITYDESCRIPTION,
+    message,
+};
